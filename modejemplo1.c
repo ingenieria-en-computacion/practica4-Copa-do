d92da4810810ec15831bd2a3ee1f07560e9ad747
Andrade Copado Emiliano <emiliano.andradecopado@gmail.com>
2025-03-06 10:24:26 -0600

    EjemplosModificados

---
+++ modejemplo1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *arreglo, num, cont;
+    printf("¿Cuántos elementos tiene el conjunto?\n");
+    scanf("%d", &num);
+    arreglo = (int*)malloc(num *sizeof(int));
+    if(arreglo != NULL){
+        printf("Vector reservado:\n\t");
+        for(cont =0; cont <num; cont++){
+            printf("\t%d %p", *(arreglo + cont));            
+        }
+        printf("\t\n");
+        printf("Liberando el espacio reservado\n");
+        free(arreglo);        
+    }
+    return 0;
+}
