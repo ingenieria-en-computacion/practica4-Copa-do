4203f26d9761561147f28c69a41ddb334fd1acdc
Co_pado <emiliano.andradecopado@gmail.com>
2025-03-08 14:17:25 -0600

    Ejercicio Uno

--- ejercicios/ejercicio1.c
+++ ejercicios/ejercicio1.c
@@ -3,26 +3,36 @@
 
 int main() {
     int n;
-    printf("Ingrese el tamaño del arreglo: ");
+    //pedir el valor de n
+    printf("Ingrese el tamaño del arreglo:\n ");
+    scanf("%d", &n);
     
-
-    int *arr;
-    if (arr == NULL) {
-        printf("Error: No se pudo asignar memoria.\n");
+    if (n <= 0) {
+        printf("Error: El tamaño debe ser mayor que 0 .\n");
+        return 1;
+    }
+    //se usa malloc para reservar memoria
+    int *arreglo = (int *)malloc(n * sizeof(int));
+    if (arreglo == NULL) {
+        printf("No se pudo asignar memoria.\n");
         return 1;
     }
 
-    printf("Ingrese %d valores:\n", n);
-    for (int i = 0; i < n; i++) {
-        
-    }
 
-    printf("El arreglo es: ");
+    //solicitar los valores del arreglo
+    printf("ingrese %d valores:\n", n);
     for (int i = 0; i < n; i++) {
-        
+        printf("valor %d: ", i + 1);
+        scanf("%d", &arreglo[i]);
+    }
+    // mostrar valores del arreglo
+    printf("el arreglo es: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arreglo[i]);
     }
     printf("\n");
-
-    
+   
+   //liberar la memoria
+    free(arreglo);
     return 0;
 }
